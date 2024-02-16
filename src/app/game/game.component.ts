8c489bf2ac0352904c08f3361e5daf2bd789570a
Werbung06 <Werbung06@skuvgele.local>
2024-02-16 10:57:33 +0100

    add game.ts

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule,],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
@@ -12,8 +13,14 @@ import { Component } from '@angular/core';
 export class GameComponent {
 
   pickCardAniamtion = false;
+  game!: Game;
 
   takeCard(){
     this.pickCardAniamtion = true;
+  }
+
+  newGame(){
+    this.game = new Game();
+    console.log(this.game);
   }
 }
